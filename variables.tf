04327747a4e4c1dde3f2487e6a04fef676ffe2a2
lenvolk <lvolkov@outlook.com>
2020-01-15 20:10:23 +0000

    missing region var

--- variables.tf
+++ variables.tf
@@ -4,3 +4,5 @@ variable "env"          { default = "Development" }
 variable "vnet_type"    { default = "unset" }
 variable "os"    { default = "unset" }
 variable "vm_type"    { default = "unset" }
+variable "region"    { default = "unset" }
+
