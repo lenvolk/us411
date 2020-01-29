ef95bc502348107ccc3a63e283f93501e7ae6bc8
lenvolk <52181728+lenvolk@users.noreply.github.com>
2020-01-29 10:00:25 -0800

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,7 +1,7 @@
 variable "env"          { default = "Development" }
 
-variable "vnet_type"    { default = null }
-variable "os"           { default = null }
-variable "vm_type"      { default = null }
-variable "region"       { default = null }
+variable "vnet_type"    { type    = string default = null }
+variable "os"           { type    = string default = null }
+variable "vm_type"      { type    = string default = null }
+variable "region"       { type    = string default = null }
 
