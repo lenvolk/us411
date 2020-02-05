b8e93c98251a10a9ffb8541bb8ee6dbf2214f0f8
lenvolk <52181728+lenvolk@users.noreply.github.com>
2020-02-05 13:27:52 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -2,4 +2,4 @@ variable "env"          { default = "dev" }
 variable "vnet_type"    { default = "main" }
 variable "os"           { default = "win" }
 variable "vm_type"      { default = "web" }
-variable "region"       { default = "eastus" }
+variable "location"     { default = "eastus" }
